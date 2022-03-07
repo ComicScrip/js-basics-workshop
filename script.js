9281835b10819adbcef044ac528033eabccf3601
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-03-07 11:13:52 +0100

    js basics workshop part 1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello');
+do {
+  const firstValue = parseInt(prompt('first value ?'), 10);
+  const operator = prompt('operator ?');
+  const secondValue = parseInt(prompt('secondValue ?'), 10);
+
+  switch (operator) {
+    case '+':
+      console.log(firstValue + secondValue);
+      break;
+    case '-':
+      console.log(firstValue - secondValue);
+      break;
+    case '*':
+      console.log(firstValue * secondValue);
+      break;
+    case '/':
+      console.log(firstValue / secondValue);
+      break;
+    default:
+      console.log('Invalid operator');
+  }
+} while (confirm('Do you want to do another operation ?'));
