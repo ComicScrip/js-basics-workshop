14605305ccd2bad8f17dc1eda4bc21ed3b3aea20
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-03-07 16:24:21 +0100

    part 1 modified

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-do {
-  const firstValue = parseInt(prompt('first value ?'), 10);
-  const operator = prompt('operator ?');
-  const secondValue = parseInt(prompt('secondValue ?'), 10);
+const firstValue = parseInt(prompt('first value ?'), 10);
+const operator = prompt('operator ?');
+const secondValue = parseInt(prompt('second value ?'), 10);
 
-  switch (operator) {
-    case '+':
-      console.log(firstValue + secondValue);
-      break;
-    case '-':
-      console.log(firstValue - secondValue);
-      break;
-    case '*':
-      console.log(firstValue * secondValue);
-      break;
-    case '/':
-      console.log(firstValue / secondValue);
-      break;
-    default:
-      console.log('Invalid operator');
-  }
-} while (confirm('Do you want to do another operation ?'));
+let result = null;
+
+switch (operator) {
+  case '+':
+    result = firstValue + secondValue;
+    break;
+  case '-':
+    result = firstValue - secondValue;
+    break;
+  case '*':
+    result = firstValue * secondValue;
+    break;
+  case '/':
+    result = firstValue / secondValue;
+    break;
+  default:
+    console.log('invalid operator');
+}
+
+if (typeof result === 'number' && !isNaN(result)) {
+  alert(`${firstValue} ${operator} ${secondValue} = ${result}`);
+} else {
+  alert('The operation could not be performed, please try again');
+}
