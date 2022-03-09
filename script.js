fa9d97b8b9fd6e70620089d40b9d4fdcd56f1e4e
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-03-09 14:27:12 +0100

    add comments

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-// DATA & FUNCTIONS
-const operationHistory = [];
+// FUNCTIONS
 
-function addToHistory(operation) {
+function addToHistory(history, operation) {
+  // creating a new empty object
   const newEntry = {};
+  // putting the current date into the object
   newEntry.date = new Date();
+  // putting the operation into the object
   newEntry.operation = operation;
-  operationHistory.push(newEntry);
+  // adding this object at the end of the list of completed operations
+  history.push(newEntry);
 }
 
-function logHistory() {
-  for (let i = 0; i < operationHistory.length; i += 1) {
-    const entry = operationHistory[i];
+function logHistory(history) {
+  // iterating over the array containing objects describing completed operations
+  for (let i = 0; i < history.length; i += 1) {
+    // for each operation we get the object describing the latter from the list
+    const entry = history[i];
+    // we print the date, the number and the result of the operation to the console
     console.log(`[${entry.date}] operation #${i} : ${entry.operation}`);
   }
 }
 
-function computeResult(a, b, operator) {
-  if (operator === '+') return a + b;
-  if (operator === '-') return a - b;
-  if (operator === '*') return a * b;
-  if (operator === '/') return a / b;
+function computeResult(firstNumber, secondNumber, operator) {
+  // we return the result according to the type of operation to perform
+  if (operator === '+') return firstNumber + secondNumber;
+  if (operator === '-') return firstNumber - secondNumber;
+  if (operator === '*') return firstNumber * secondNumber;
+  if (operator === '/') return firstNumber / secondNumber;
+  // if the operator is not a valid operator (+, -, *, /), we display a wrning in the console and return null
   console.warn('invalid operator');
   return null;
 }
@@ -27,6 +35,7 @@ function computeResult(a, b, operator) {
 function getValidNumber(message) {
   let input;
 
+  // We ask the user to enter something until it's a valid number
   do {
     input = parseFloat(prompt(message));
   } while (typeof input !== 'number' || isNaN(input));
@@ -35,24 +44,34 @@ function getValidNumber(message) {
 }
 
 function getOperator() {
+  // we define the list of valid operators
+  const allowedOperators = ['+', '-', '*', '/'];
   let input;
 
+  // we ask the user to enter something until it is a valid operator
   do {
     input = prompt('operator ?');
-  } while (!['+', '-', '*', '/'].includes(input));
+  } while (!allowedOperators.includes(input));
 
   return input;
 }
 
 // MAIN PROGRAM
+const operationHistory = [];
+
 do {
+  // getting user inputs
   const firstValue = getValidNumber('first value ?');
   const operator = getOperator();
   const secondValue = getValidNumber('second value ?');
+  // computing the result
   const result = computeResult(firstValue, secondValue, operator);
-  const operationWithResult = `${firstValue} ${operator} ${secondValue} = ${result}`;
-  alert(operationWithResult);
-  addToHistory(operationWithResult);
+  // displaying the operation along with the result in a popup
+  const completedOperation = `${firstValue} ${operator} ${secondValue} = ${result}`;
+  alert(completedOperation);
+  // adding the completed operation in the hisotry
+  addToHistory(operationHistory, completedOperation);
 } while (confirm('Do you want to perform another operation ?'));
 
-logHistory();
+// logging the operation hisotry to the console when the program ends
+logHistory(operationHistory);
